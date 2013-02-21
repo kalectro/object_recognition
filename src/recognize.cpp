559b87eac51a7a0dfc80156275a1aee7155c8751
Kai Franke <kai@kaifranke.com>
2013-02-20 20:48:07 -0800

    works but veeeery slow

--- src/recognize.cpp
+++ src/recognize.cpp
@@ -6,6 +6,39 @@
 
 #include "recognize.h"
 
+double
+computeCloudResolution (const pcl::PointCloud<PointType>::ConstPtr &cloud)
+{
+  double res = 0.0;
+  int n_points = 0;
+  int nres;
+  std::vector<int> indices (2);
+  std::vector<float> sqr_distances (2);
+  pcl::search::KdTree<PointType> tree;
+  tree.setInputCloud (cloud);
+
+  for (size_t i = 0; i < cloud->size (); ++i)
+  {
+    if (! pcl_isfinite ((*cloud)[i].x))
+    {
+      continue;
+    }
+    //Considering the second neighbor since the first is the point itself.
+    nres = tree.nearestKSearch (i, 2, indices, sqr_distances);
+    if (nres == 2)
+    {
+      res += sqrt (sqr_distances[1]);
+      ++n_points;
+    }
+  }
+  if (n_points != 0)
+  {
+    res /= n_points;
+  }
+  return res;
+}
+
+
 void world_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 {
 	scene               = PointCloud::Ptr    (new PointCloud    ());
@@ -27,7 +60,7 @@ void world_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 	uniform_sampling.compute (sampled_indices);
 	pcl::copyPointCloud (*scene, sampled_indices.points, *scene_keypoints);
 	std::cout << "Scene total points: " << scene->size () << "; Selected Keypoints: " << scene_keypoints->size () << std::endl;
-
+cout << "asdf1" << endl;
     //
     // Extract descriptors
     //
@@ -35,47 +68,70 @@ void world_cb (const sensor_msgs::PointCloud2ConstPtr& input)
     descr_est.setInputNormals (scene_normals);
     descr_est.setSearchSurface (scene);
     descr_est.compute (*scene_descriptors);
+cout << "asdf2" << endl;
 }
 
 void object_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 {
-    model               = PointCloud::Ptr    (new PointCloud    ());
-    model_keypoints     = PointCloud::Ptr    (new PointCloud    ());
-    model_normals       = NormalCloud::Ptr   (new NormalCloud   ());
-    model_descriptors   = DesciptorCloud::Ptr(new DesciptorCloud());
-    
-    pcl::fromROSMsg(*input, *model);
-    
-    // compute normals
-    norm_est.setInputCloud (model);
-    norm_est.compute (*model_normals);
+	model               = PointCloud::Ptr    (new PointCloud    ());
+	model_keypoints     = PointCloud::Ptr    (new PointCloud    ());
+	model_normals       = NormalCloud::Ptr   (new NormalCloud   ());
+	model_descriptors   = DesciptorCloud::Ptr(new DesciptorCloud());
 
-    //
-    //  Downsample object to extract keypoints
-    //
-    uniform_sampling.setInputCloud (model);
-    uniform_sampling.setRadiusSearch (model_ss_);
-    uniform_sampling.compute (sampled_indices);
-    pcl::copyPointCloud (*model, sampled_indices.points, *model_keypoints);
-    std::cout << "Model total points: " << model->size () << "; Selected Keypoints: " << model_keypoints->size () << std::endl;
-    
-    //
-    // Extract descriptors
-    //
-    descr_est.setInputCloud (model_keypoints);
-    descr_est.setInputNormals (model_normals);
-    descr_est.setSearchSurface (model);
-    descr_est.compute (*model_descriptors);
-    
-    
-    //
-    //  Find Model-Scene Correspondences with KdTree
-    //
-    pcl::CorrespondencesPtr model_scene_corrs (new pcl::Correspondences ());
-    
-    pcl::KdTreeFLANN<DescriptorType> match_search;
-    match_search.setInputCloud (model_descriptors);
-    
+	pcl::fromROSMsg(*input, *model);
+	cout << "asdf3" << endl;
+
+	//
+  //  Set up resolution invariance
+  //
+	float resolution = static_cast<float> (computeCloudResolution (model));
+	if (resolution != 0.0f)
+	{
+		model_ss_   *= resolution;
+		scene_ss_   *= resolution;
+		rf_rad_     *= resolution;
+		descr_rad_  *= resolution;
+		cg_size_    *= resolution;
+	}
+
+	std::cout << "Model resolution:       " << resolution << std::endl;
+	std::cout << "Model sampling size:    " << model_ss_ << std::endl;
+	std::cout << "Scene sampling size:    " << scene_ss_ << std::endl;
+	std::cout << "LRF support radius:     " << rf_rad_ << std::endl;
+	std::cout << "SHOT descriptor radius: " << descr_rad_ << std::endl;
+	std::cout << "Clustering bin size:    " << cg_size_ << std::endl << std::endl;
+
+
+
+	// compute normals
+	norm_est.setInputCloud (model);
+	norm_est.compute (*model_normals);
+cout << "asdf4" << endl;
+	//
+	//  Downsample object to extract keypoints
+	//
+	uniform_sampling.setInputCloud (model);
+	uniform_sampling.setRadiusSearch (model_ss_);
+	uniform_sampling.compute (sampled_indices);
+	pcl::copyPointCloud (*model, sampled_indices.points, *model_keypoints);
+	std::cout << "Model total points: " << model->size () << "; Selected Keypoints: " << model_keypoints->size () << std::endl;
+cout << "asdf5" << endl;
+	//
+	// Extract descriptors
+	//
+	descr_est.setInputCloud (model_keypoints);
+	descr_est.setInputNormals (model_normals);
+	descr_est.setSearchSurface (model);
+	descr_est.compute (*model_descriptors);
+	
+	//
+	//  Find Model-Scene Correspondences with KdTree
+	//
+	pcl::CorrespondencesPtr model_scene_corrs (new pcl::Correspondences ());
+	
+	pcl::KdTreeFLANN<DescriptorType> match_search;
+	match_search.setInputCloud (model_descriptors);
+		
 	// For each scene keypoint descriptor
 	// find nearest neighbor into the model keypoints descriptor cloud 
 	// and add it to the correspondences vector
@@ -96,7 +152,7 @@ void object_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 			model_scene_corrs->push_back (corr);
 		}
 	}
-    std::cout << "Correspondences found: " << model_scene_corrs->size () << std::endl;
+		std::cout << "Correspondences found: " << model_scene_corrs->size () << std::endl;
     
     
     //
@@ -116,26 +172,26 @@ void object_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 	//gc_clusterer.cluster (clustered_corrs);
 	gc_clusterer.recognize (rototranslations, clustered_corrs);
     
-    //
-    //  Output results
-    //
-    std::cout << "Model instances found: " << rototranslations.size () << std::endl;
-    for (size_t i = 0; i < rototranslations.size (); ++i)
-    {
-        std::cout << "\n    Instance " << i + 1 << ":" << std::endl;
-        std::cout << "        Correspondences belonging to this instance: " << clustered_corrs[i].size () << std::endl;
-        
-        // Print the rotation matrix and translation vector
-        Eigen::Matrix3f rotation = rototranslations[i].block<3,3>(0, 0);
-        Eigen::Vector3f translation = rototranslations[i].block<3,1>(0, 3);
-        
-        printf ("\n");
-        printf ("            | %6.3f %6.3f %6.3f | \n", rotation (0,0), rotation (0,1), rotation (0,2));
-        printf ("        R = | %6.3f %6.3f %6.3f | \n", rotation (1,0), rotation (1,1), rotation (1,2));
-        printf ("            | %6.3f %6.3f %6.3f | \n", rotation (2,0), rotation (2,1), rotation (2,2));
-        printf ("\n");
-        printf ("        t = < %0.3f, %0.3f, %0.3f >\n", translation (0), translation (1), translation (2));
-    }
+  //
+  //  Output results
+  //
+  std::cout << "Model instances found: " << rototranslations.size () << std::endl;
+  for (size_t i = 0; i < rototranslations.size (); ++i)
+  {
+      std::cout << "\n    Instance " << i + 1 << ":" << std::endl;
+      std::cout << "        Correspondences belonging to this instance: " << clustered_corrs[i].size () << std::endl;
+      
+      // Print the rotation matrix and translation vector
+      Eigen::Matrix3f rotation = rototranslations[i].block<3,3>(0, 0);
+      Eigen::Vector3f translation = rototranslations[i].block<3,1>(0, 3);
+      
+      printf ("\n");
+      printf ("            | %6.3f %6.3f %6.3f | \n", rotation (0,0), rotation (0,1), rotation (0,2));
+      printf ("        R = | %6.3f %6.3f %6.3f | \n", rotation (1,0), rotation (1,1), rotation (1,2));
+      printf ("            | %6.3f %6.3f %6.3f | \n", rotation (2,0), rotation (2,1), rotation (2,2));
+      printf ("\n");
+      printf ("        t = < %0.3f, %0.3f, %0.3f >\n", translation (0), translation (1), translation (2));
+  }
 
 }
 
