3e72653ae3abeb5a7e614751cdbb0a63cb2e31cd
Kai Franke <kai@kaifranke.com>
2013-02-24 20:06:28 -0800

    added executable to find descriptors of an object and publish them, publishing not really working yet

---
+++ src/pcd_to_descriptors.cpp
@@ -0,0 +1,93 @@
+/*
+ *
+ *  Created on: Feb 24, 2013
+ *      Author: Kai Franke
+ */
+
+#include "pcd_to_descriptors.h"
+
+int main(int argc, char **argv)
+{
+	//
+	// take care of the ROS stuff
+	//
+	ros::init(argc, argv, "cloud_descriptor");
+	ros::NodeHandle nh("~");
+
+	// Create a ROS publisher for the output model coefficients
+	pub_keypoints   = nh.advertise<sensor_msgs::PointCloud2> ("keypoints", 1);
+	//pub_descriptors = nh.advertise<pcl::PointCloud<pcl::SHOT352>::Ptr> ("descriptors", 1);
+
+	// If parameter pcd_path was not specified
+	if (!nh.getParam("pcd_path", pcd_path))
+	{
+		ROS_ERROR("Private parameter pcd_path not found");
+		ROS_ERROR("Usage: rosrun object_recognition pcd_descriptor _pcd_path:=/path/to/pcd");
+		return -1;
+	}
+
+	
+	//
+	// load the pcd into the point cloud
+	//
+	if (pcl::io::loadPCDFile (pcd_path, *cloud) == -1) 
+	{
+		PCL_ERROR ("Couldn't read file %s \n", pcd_path.c_str());
+		return (-1);
+	}
+
+
+	//
+	// create all neccessary objects
+	//
+	cloud               = PointCloud::Ptr    (new PointCloud    ());
+	cloud_keypoints     = PointCloud::Ptr    (new PointCloud    ());
+	cloud_normals       = NormalCloud::Ptr   (new NormalCloud   ());
+	cloud_descriptors   = DesciptorCloud::Ptr(new DesciptorCloud());
+
+	// Algorithm params
+	cloud_ss_ = 0.01;
+	rf_rad_ = 0.015;
+	descr_rad_ = 0.02;
+
+
+	//
+	// compute normals
+	//
+	cout << "... computing normals from cloud ..." << endl;
+	norm_est_cloud.setKSearch (10);
+	norm_est_cloud.setInputCloud (cloud);
+	norm_est_cloud.compute (*cloud_normals);
+
+
+	//
+	//  Downsample cloud to extract keypoints
+	//
+	cout << "... downsampling cloud ..." << endl;
+	uniform_sampling_cloud.setInputCloud (cloud);
+	uniform_sampling_cloud.setRadiusSearch (cloud_ss_);
+	uniform_sampling_cloud.compute (sampled_indices_cloud);
+	pcl::copyPointCloud (*cloud, sampled_indices_cloud.points, *cloud_keypoints);
+	std::cout << "Cloud total points: " << cloud->size () << "; Selected Keypoints: " << cloud_keypoints->size () << std::endl;
+
+
+	//
+	// Extract descriptors
+	//
+	cout << "... extracting descriptors from cloud ..." << endl;
+	descr_est_cloud.setInputCloud (cloud_keypoints);
+	descr_est_cloud.setRadiusSearch (descr_rad_);
+	descr_est_cloud.setInputNormals (cloud_normals);
+	descr_est_cloud.setSearchSurface (cloud);
+	descr_est_cloud.compute (*cloud_descriptors);
+
+	//
+	// Publish Keypoints and Descriptors
+	//
+	pcl::toROSMsg(*cloud_keypoints, *output);
+	pub_keypoints.publish(*output);
+	//pub_descriptors.publish(*cloud_descriptors);
+
+	ros::spinOnce();
+	return 0;
+}
---
+++ src/pcd_to_descriptors.h
@@ -0,0 +1,50 @@
+/*
+ * pcd_to_descriptors.h
+ *
+ *  Created on: Feb 24, 2013
+ *      Author: Kai Franke
+ * This program reads a pcd file and publishes its keypoint descriptors
+ */
+
+#include "ros/ros.h"
+#include <sensor_msgs/PointCloud2.h>
+#include <stdio.h>
+#include <string>
+#include <pcl/point_cloud.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/features/normal_3d_omp.h>
+#include <pcl/features/shot_omp.h>
+#include <pcl/features/board.h>
+#include <pcl/keypoints/uniform_sampling.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/kdtree/impl/kdtree_flann.hpp>
+#include <pcl/common/transforms.h>
+
+typedef pcl::PointXYZRGBA PointType;
+typedef pcl::Normal NormalType;
+typedef pcl::SHOT352 DescriptorType;
+typedef pcl::PointCloud<PointType> PointCloud;
+typedef pcl::PointCloud<NormalType> NormalCloud;
+typedef pcl::PointCloud<DescriptorType> DesciptorCloud;
+
+using namespace std;
+
+// Initialize Publisher for keypoints and descriptors
+ros::Publisher pub_keypoints, pub_descriptors;
+
+string pcd_path;
+
+//Algorithm params
+float cloud_ss_,rf_rad_, descr_rad_ ;
+
+// Point clouds for object, world and its normals, keypoints and descriptors
+PointCloud::Ptr cloud;
+PointCloud::Ptr cloud_keypoints;
+NormalCloud::Ptr cloud_normals;
+DesciptorCloud::Ptr cloud_descriptors;
+sensor_msgs::PointCloud2::Ptr output;
+
+pcl::NormalEstimationOMP<PointType, NormalType> norm_est_cloud;
+pcl::PointCloud<int> sampled_indices_cloud;
+pcl::UniformSampling<PointType> uniform_sampling_cloud;
+pcl::SHOTEstimationOMP<PointType, NormalType, DescriptorType> descr_est_cloud;
